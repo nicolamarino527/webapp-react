39fae6c9a7ec1996b18689434262962e84e9ffd9
nicolamarino <nicolamarino527@gmail.com>
2025-03-12 21:54:41 +0100

    [feat] axios install add homepage call

--- components/MoviesCard.jsx
+++ components/MoviesCard.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router"
 
-const MovieCard = () => {
+const MovieCard = ({ movieProp }) => {
+
+    const { id, title, director, image } = movieProp;
+
+
     return (
         <div className="card" style={{ width: '23em' }}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Eraserhead.jpg/640px-Eraserhead.jpg" className="card-img-top" alt="..." />
+            <img src={image} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">film</h5>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">Direttore: {director}</p>
                 <p className="card-text">
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                 </p>
-                <Link to="movies/1" className="btn btn-primary">informazioni</Link>
+                <Link to={`/movies/${id}`} className="btn btn-primary">informazioni</Link>
             </div>
         </div>
     )
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,16 +1,45 @@
+// axios
+import axios from "axios"
+
+import { useEffect, useState } from "react"
+
 import MovieCard from "../components/MoviesCard"
 
 const HomePage = () => {
+
+    // settiamo lo stato per il componente
+    const [movies, setMovies] = useState([]);
+
+    // funzione di chiamata api 
+    const fetchMovies = () => {
+        axios.get("http://localhost:3000/api/movies")
+            .then(
+                res => {
+                    setMovies(res.data)
+                }
+            )
+            .catch(err => console.log(err)
+            )
+    }
+    useEffect(fetchMovies, [])
+
+    const renderMovies = () => {
+        return movies.map(movie => {
+            return (
+                <div key={movie.id}>
+                    <MovieCard movieProp={movie} />
+                </div>
+            )
+        }
+        )
+    }
+
     return (
         <div>
             <h1>Benvenuto nella pagina della home </h1>
             <p>qui troverai tutti i tuoi films</p>
             <div className="movie-container">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {renderMovies()}
             </div>
         </div>
     )
